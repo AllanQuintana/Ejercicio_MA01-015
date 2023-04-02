e172dfba64467950dfd32cd497e44caae33ae121
ALLAN SEBASTIAN QUINTANA ABARCA <allan.quintana@gmail.com>
2023-04-02 14:03:51 -0400

    programa optimizado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        Write("Ingrese la figura (círculo, cuadrado, triángulo): ");
+        string figura = ReadLine().ToLower();
+
+        double area = 0;
+        switch (figura)
+        {
+            case "círculo":
+                Write("Ingrese el radio: ");
+                double radio = double.Parse(ReadLine());
+                area = Math.PI * radio * radio;
+                break;
+
+            case "cuadrado":
+                Write("Ingrese la medida del lado: ");
+                double lado = double.Parse(ReadLine());
+                area = lado * lado;
+                break;
+
+            case "triángulo":
+                Write("Ingrese la medida de la base: ");
+                double baseTriangulo = double.Parse(ReadLine());
+                Write("Ingrese la medida de la altura: ");
+                double alturaTriangulo = double.Parse(ReadLine());
+                area = (baseTriangulo * alturaTriangulo) / 2;
+                break;
+
+            default:
+                WriteLine("La figura ingresada no es válida.");
+                return;
+        }
+
+        WriteLine($"El área del {figura} es: {area}");
+    }
+}
